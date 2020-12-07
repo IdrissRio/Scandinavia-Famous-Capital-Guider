6770dd82b032dc0b0d6ef5423795e7e522a1ea0c
Y <47159710+grantgy@users.noreply.github.com>
2020-12-07 01:50:58 +0100

    Update interaction.kt

--- skill/CaptialGuide/src/main/kotlin/furhatos/app/captialguide/flow/interaction.kt
+++ skill/CaptialGuide/src/main/kotlin/furhatos/app/captialguide/flow/interaction.kt
@@ -14,7 +14,12 @@ import furhatos.util.Language
 val Start: State = state(Interaction) {
 
     onEntry {
-        furhat.say("Hi there!")
+        random(
+                {furhat.say("Hi there!")},
+                {furhat.say("Hello there!")},
+                {furhat.say("Hello!")},
+                {furhat.say("Hi!")}
+        )
         val lastCity = available_cities.last()
         furhat.say("I have information about ${available_cities.take(available_cities.size - 1).joinToString(" ")} and $lastCity")
         goto(CityOptions)
@@ -26,23 +31,56 @@ val End: State = state {
     onEntry {
         val bookings = users.current.information.bookings
         if (bookings.isNotEmpty()) {
-            furhat.say("You have booked")
+            random(
+                    {furhat.say("You have booked")},
+                    {furhat.say("You have already booked")},
+                    {furhat.say("Now you have")},
+                    {furhat.say("You have")}
+            )
             users.current.information.bookings.forEach {
                 furhat.say(it)
             }
         }
-        furhat.say("Goodbye and have a nice day")
+        random(
+                {furhat.say("Goodbye and have a nice day")},
+                {furhat.say("Bye and have a nice day")},
+                {furhat.say("Byebye and have a nice day")},
+                {furhat.say("Have a nice day, bye")},
+                {furhat.say("Have a nice day, byebye")},
+                {furhat.say("Thanks! Goodbye!")},
+                {furhat.say("Thanks! Byebye!")},
+                {furhat.say("Byebye!")},
+                {furhat.say("Goodbye!")}
+        )
         goto(Idle)
     }
 }
 
 fun DummyCityOptions(): State = state(CityOptions) {
     onEntry {
-        furhat.ask("Is there another city you would like to visit?")
+        random(
+                {furhat.ask("Is there another city you would like to visit?")},
+                {furhat.ask("Do you want to go somewhere else?")},
+                {furhat.ask("Would you like to go somewhere else?")},
+                {furhat.ask("Do you want to visit somewhere else?")},
+                {furhat.ask("Would you like to visit somewhere else?")},
+                {furhat.ask("Do you want to go to another city?")},
+                {furhat.ask("Would you like to visit another city?")},
+                {furhat.ask("Do you want to visit another city?")}
+        )
     }
 
     onReentry {
-        furhat.ask("Is there another city you would like to visit?")
+        random(
+                {furhat.ask("Is there another city you would like to visit?")},
+                {furhat.ask("Do you want to go somewhere else?")},
+                {furhat.ask("Would you like to go somewhere else?")},
+                {furhat.ask("Do you want to visit somewhere else?")},
+                {furhat.ask("Would you like to visit somewhere else?")},
+                {furhat.ask("Do you want to go to another city?")},
+                {furhat.ask("Would you like to visit another city?")},
+                {furhat.ask("Do you want to visit another city?")}
+        )
     }
 
     onResponse<Yes> {
@@ -58,7 +96,14 @@ fun DummyCityOptions(): State = state(CityOptions) {
 val CityOptions = state(Interaction) {
 
     onEntry {
-        furhat.ask("Which city are you interested in?")
+        random(
+                {furhat.ask("Which city are you interested in?")},
+                {furhat.ask("Which city would you like to go?")},
+                {furhat.ask("where would you like to visit?")},
+                {furhat.ask("where do want to travel?")},
+                {furhat.ask("Which city do you want to visit?")},
+                {furhat.ask("Is there a city you want to visit?")}
+        )
     }
 
     onResponse<ChooseCity> {
@@ -88,9 +133,19 @@ val CityOptions = state(Interaction) {
     onResponse<ListBookedActivities> {
         val bookings = users.current.information.bookings
         if (bookings.isEmpty()) {
-            furhat.say("You have no booked activities.")
+            random(
+                    {furhat.say("You have no booked activities.")},
+                    {furhat.say("You have not booked any activities.")},
+                    {furhat.say("No activity has been booked.")},
+                    {furhat.say("There is no activity has been booked.")}
+            )
         } else {
-            furhat.say("You have booked")
+            random(
+                    {furhat.say("You have booked")},
+                    {furhat.say("You have already booked")},
+                    {furhat.say("Now you have")},
+                    {furhat.say("You have")}
+            )
             users.current.information.bookings.forEach {
                 furhat.say(it)
             }
@@ -103,11 +158,18 @@ val CityOptions = state(Interaction) {
 fun Options(): State = state(CityOptions) {
 
     onEntry {
-        furhat.ask("Would you like some information about ${users.current.information.city.toName()} or book an activity?")
+        random(
+                {furhat.ask("Would you like some information about ${users.current.information.city.toName()} or book an activity?")},
+                {furhat.ask("Do you want to book an activity or know some information about ${users.current.information.city.toName()}?")},
+                {furhat.ask("some information about ${users.current.information.city.toName()} or book an activity?")}
+        )
     }
 
     onReentry {
-        furhat.ask("Would you like some more information about ${users.current.information.city.toName()} or book an activity?")
+        random(
+                {furhat.ask("Would you like some information about ${users.current.information.city.toName()} or book an activity?")},
+                {furhat.ask("Do you want to book an activity or know some information about ${users.current.information.city.toName()}?")}
+        )
     }
 
     onResponse<BookActivity> {
@@ -134,21 +196,45 @@ fun SuggestBookings(city: CityWithBooking): State = state(Options()) {
     onEntry {
         val activities = users.current.information.city.activities
         val lastActivity = activities.last()
-        furhat.say("Your activity options in ${city.city.name} are ${activities.take(activities.size - 1).joinToString(", ")} or $lastActivity!")
-        furhat.ask("Is there an activity you would like to book?")
+        random(
+                {furhat.say("Your activity options in ${city.city.name} are ${activities.take(activities.size - 1).joinToString(", ")} or $lastActivity!")},
+                {furhat.say("In ${city.city.name}, we have ${activities.take(activities.size - 1).joinToString(", ")} or $lastActivity!")},
+                {furhat.say("We have ${activities.take(activities.size - 1).joinToString(", ")} or $lastActivity!, in ${city.city.name}")},
+                {furhat.say("Your choices are ${activities.take(activities.size - 1).joinToString(", ")} or $lastActivity!, in ${city.city.name}")},
+                {furhat.say("You can choose ${activities.take(activities.size - 1).joinToString(", ")} or $lastActivity!, in ${city.city.name}")}
+                )
+        random(
+                {furhat.ask("Is there an activity you would like to book?")},
+                {furhat.ask("Which one do you want to book?")}
+        )
     }
 
     onReentry {
         if (users.current.information.bookings.isNotEmpty()) {
-            furhat.ask("Would you like to book another activity?")
+            random(
+                    {furhat.ask("Would you like to book another activity?")},
+                    {furhat.ask("Would you like to book one more?")},
+                    {furhat.ask("One more?")},
+                    {furhat.ask("Any others?")},
+                    {furhat.ask("Is that done?")}
+            )
         } else {
-            furhat.ask("Is there an activity in ${city.city.name} you would like to book?")
+            random(
+                    {furhat.ask("Is there an activity in ${city.city.name} you would like to book?")},
+                    {furhat.ask("Which activity do you want to book ${city.city.name} ?")},
+                    {furhat.ask("which one would you like to book in ${city.city.name}?")}
+            )
         }
 
     }
 
     onResponse(city.getIntents()) {
-        furhat.say("Done! ${it.intent} has been booked!")
+        random(
+                {furhat.say("Done! ${it.intent} has been booked!")},
+                {furhat.say("All right! Now you have booked ${it.intent}!")},
+                {furhat.say("All right! ${it.intent} has been booked!")},
+                {furhat.say("Now you have ${it.intent}!")}
+        )
         users.current.information.bookings.add(it.intent.toString())
         reentry()
     }
@@ -179,11 +265,17 @@ fun RecievedCityAndActivity(city: City, activity: Activity?) = state {
                     furhat.say("$activity has been booked in ${city.toName()}")
                     users.current.information.bookings.add(activity.toString())
                 } else {
-                    furhat.say("Sorry, I do not have a record of that activity in ${city.toName()}")
+                    random(
+                            {furhat.say("Sorry, I do not have a record of that activity in ${city.toName()}")},
+                            {furhat.say("Sorry, I do not have it in ${city.toName()}")}
+                    )
                 }
                 goto(Options())
             } else {
-                furhat.say("Sorry, I did not catch what activity you would like to book in ${city.toName()}")
+                random(
+                        {furhat.say("Sorry, I did not catch what activity you would like to book in ${city.toName()}")},
+                        {furhat.say("Sorry, which activity in ${city.toName()}? I did not catch it")}
+                )
                 goto(SuggestBookings(city = city))
             }
         } catch (e: java.lang.ClassNotFoundException) {
@@ -198,7 +290,13 @@ fun RecievedCityAndActivity(city: City, activity: Activity?) = state {
 val CityInformation = state {
 
     onEntry {
-        furhat.say("I will tell you a fact")
+        random(
+                {furhat.say("I will tell you a fact")},
+                {furhat.say("I will tell you something interesting")},
+                {furhat.say("Here is something interesting")},
+                {furhat.say("Here is a piece of fact")},
+                {furhat.say("Here is a piece of information")}
+        )
         val city = users.current.information.city
         val fact = city.facts.shuffled().takeLast(1)[0]
         furhat.say(fact)
