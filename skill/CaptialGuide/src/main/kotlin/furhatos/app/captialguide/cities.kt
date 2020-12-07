50105ac5ca2852a2b59de0161fb34d4618563743
mandyxiaomeng <44643765+mandyxiaomeng@users.noreply.github.com>
2020-12-07 17:38:54 +0100

    Update cities.kt

--- skill/CaptialGuide/src/main/kotlin/furhatos/app/captialguide/cities.kt
+++ skill/CaptialGuide/src/main/kotlin/furhatos/app/captialguide/cities.kt
@@ -33,12 +33,15 @@ open class CityWithBooking(val city: City) {
 class Stockholm(city: City) : CityWithBooking(city) {
     override val facts = listOf("Stockholm was founded in twelve fifty two (1252)",
             "The population is ${city.population}",
-            "It has the most handsome men in the world!!"
+            "It has the most awesome group in the world!!",
+            "It became the venue for the award of the first Nobel Prizes, in the year 1901.",
+            "It’s subway is also known as the world’s longest art gallery, with the majority of its stations being adorned with paintings, sculptures and mosaics.",
+            "It is sometimes referred to as ‘Venice of the North’, thanks to its beautiful buildings and exquisite architecture, abundant open water and numerous parks."
     )
 
     class Activity : EnumEntity(stemming = true, speechRecPhrases = true) {
         override fun getEnum(lang: Language): List<String>{
-            return listOf("castle", "old town", "archipelago", "city hall")
+            return listOf("Vasa Museum","castle", "old town", "archipelago", "city hall")
         }
     }
 
@@ -47,6 +50,7 @@ class Stockholm(city: City) : CityWithBooking(city) {
     }
 
     override val activities = listOf(
+            "Visit the Vasa Museum",
             "Visit the castle",
             "Go to the archipelago",
             "Go to the city hall",
@@ -54,14 +58,17 @@ class Stockholm(city: City) : CityWithBooking(city) {
 }
 
 class Oslo(city: City) : CityWithBooking(city) {
-    override val facts = listOf("Stockholm was founded in twelve fifty two (1252)",
+    override val facts = listOf("Oslo was founded around ten fourty nine (1049)",
             "The population is ${city.population}",
-            "It has the most handsome men in the world!!"
+            "Oslo residents are Norway’s healthiest!!",
+            "It used to be called Christiania",
+            "The Nobel Peace Prize is awarded in Oslo",
+            "Despite the fact that the nearest wild tigers are thousands of miles away, Oslo is known as Tigerstaden, or the Tiger City."
     )
 
     class Activity : EnumEntity(stemming = true, speechRecPhrases = true) {
         override fun getEnum(lang: Language): List<String>{
-            return listOf("castle", "old town", "archipelago", "city hall")
+            return listOf("National Gallery", "royal palace", "Ski Museum", "city hall")
         }
     }
 
@@ -70,8 +77,59 @@ class Oslo(city: City) : CityWithBooking(city) {
     }
 
     override val activities = listOf(
-            "Visit the castle",
-            "Go to the archipelago",
-            "Go to the city hall",
-            "Visit the Old Town")
+            "Visit the National Gallery",
+            "Go to the royal palace",
+            "Go to the the Holmenkollen Ski Museum",
+            "Visit the city hall")
+}
+
+class Copenhagen(city: City) : CityWithBooking(city) {
+    override val facts = listOf("Copenhagen was founded in eleven sixty seven (1167)",
+        "The population is ${city.population}",
+        "55% of Copenhageners commute to work by bicycle.",
+        "COPENHAGEN HAS THE LONGEST PEDESTRIAN SHOPPING STREET IN THE WORLD",
+        "COPENHAGEN BOASTS FIFTEEN (!) MICHELIN STARS, time to eat!",
+        "COPENHAGEN HAS A SELF-GOVERNING FREETOWN:Christiana"
+    )
+
+    class Activity : EnumEntity(stemming = true, speechRecPhrases = true) {
+        override fun getEnum(lang: Language): List<String>{
+            return listOf("Tivoli Garden", "Christiansborg Palace", "Nyhavn Harbor", "little Mermaid")
+        }
+    }
+
+    override fun getIntents() : IntentCandidate {
+        return Activity()
+    }
+
+    override val activities = listOf(
+        "Visit the Tivoli Garden",
+        "Go to the Christiansborg Palace",
+        "Go to the Nyhavn Harbor",
+        "Visit the little Mermaid")
+}
+
+class Helsinki(city: City) : CityWithBooking(city) {
+    override val facts = listOf("Helsinki was founded in fifteen fifty (1550)",
+        "The population is ${city.population}",
+        " In 2012 a special dog pool opened its doors in the east of Helsinki.",
+        "Helsinki is the world’s coldest capital, with a yearly average temperature not exceeding 0 °C, and 51 days per year no sun can be seen in the sky.",
+        "JINGLE BELLS, JINGLE BELLS, JINGLE ALL THE WAY! Santa Claus was born in Finland!!"
+    )
+
+    class Activity : EnumEntity(stemming = true, speechRecPhrases = true) {
+        override fun getEnum(lang: Language): List<String>{
+            return listOf("Kiasma museum", "amusement park", "churchs", "market")
+        }
+    }
+
+    override fun getIntents() : IntentCandidate {
+        return Activity()
+    }
+
+    override val activities = listOf(
+        "Visit the Kiasma Museum",
+        "Go to the Linnanmaki Amusement Park",
+        "Go to the three famous churchs:Uspenski Cathedral,Helsinki Cathedral, and Temppeliaukio Kirkko",
+        "Visit the Kauppatori Market")
 }
